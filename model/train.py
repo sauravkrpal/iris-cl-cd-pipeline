ad2ff5e058018043d9428c0196c3d9f1fb1f6d8d
NidhiGRaheja <nidhisr.da@gmail.com>
2025-08-20 01:40:58 +0530

    Initial Commit 1.1

--- model/train.py
+++ model/train.py
@@ -22,4 +22,4 @@ model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 # Save the model
-joblib.dump(model, 'C:\ml-ci-cd-pipeline\iris_model.pkl')
+joblib.dump(model, 'iris_model.pkl')
